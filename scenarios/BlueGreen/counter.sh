5d94e3c20f6fa47cf3e936c58e8bd045845ab301
RuncibleSpoon <robert.haynes@gmail.com>
2019-08-30 17:16:30 +0000

    more blue green ratio stuff

--- scenarios/BlueGreen/counter.sh
+++ scenarios/BlueGreen/counter.sh
@@ -4,14 +4,28 @@ COUNT=0
 TESTS=100
 BLUE=0
 GREEN=0
+HOST="bigip"
+echo "###########################################"
+echo "###                                     ###"
+echo "### Quick Blue/Green Deployment Counter ###"
+echo "###                                     ###"
+echo "###########################################"
+echo "###                                     ###"
+echo "### Running $TESTS tests against $HOST     ###"
+echo "###                                     ###"
+echo "###########################################"
+echo "###                                     ###"
 while [ $COUNT -lt $TESTS ]; do
-   FOUND=`curl -s bigip | grep -c "Blue"` 	 
+   FOUND=`curl -s $HOST | grep -c "Blue"` 	 
    #echo $FOUND
    let BLUE=$BLUE+$FOUND
    #echo $BLUE
    let COUNT=COUNT+1
 done  
 let BLUE=BLUE/2
-echo "Blue Server Hit $BLUE times"
 let GREEN=TESTS-BLUE
-echo "Green Server Hit $GREEN times"
+echo "###   Blue Server Hit $BLUE times          ###"
+echo "###                                     ###"
+echo "###   Green Server Hit $GREEN times         ###"
+echo "###                                     ###"
+echo "###########################################"
